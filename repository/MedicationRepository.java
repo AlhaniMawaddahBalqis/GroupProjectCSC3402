9d1c3ac42da7114480a11d717160053f22d2a1c6
intanfarz <167157241+intanfarz@users.noreply.github.com>
2024-06-25 14:56:48 +0800

    Delete repository directory

--- repository/MedicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csc3402.lab.pharmacy.repository;
-
-import com.csc3402.lab.pharmacy.model.Medication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedicationRepository extends JpaRepository<Medication, String> {
-}
